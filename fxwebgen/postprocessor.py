bb495488651e7a16f6b370cbba7dab6e80e3bc9c
Jiří Janoušek <janousek.jiri@gmail.com>
2018-08-03 21:33:29 +0200

    Add legacy support for Pelican links
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- fxwebgen/postprocessor.py
+++ fxwebgen/postprocessor.py
@@ -19,11 +19,32 @@ def replace_absolute_links(page: Page, tree: BeautifulSoup) -> None:
         elm['src'] = page.webroot + "/" + elm.get('src')[1:].lstrip('/')
 
 
+PELICAN_LINK_RE = re.compile(r"\{filename\}(\.?)(.+)\.md(.*)")
+
+
+def replace_pelican_links(page: Page, tree: BeautifulSoup) -> None:
+    for link in tree.find_all(href=PELICAN_LINK_RE):
+        url = link.get('href')
+        print(f'Warning: {page.source}: Pelican links are deprecated: "{url}".')
+        match = PELICAN_LINK_RE.search(url)
+        assert match
+        dot = match.group(1)
+        filename = match.group(2)
+        rest = match.group(3)
+        if dot:
+            link['href'] = dot + filename + ".html" + rest
+        else:
+            link['href'] = page.webroot + "/" + filename + ".html" + rest
+
+
 class PostProcessor:
     post_processors: List[Callable[[Page, BeautifulSoup], None]]
 
     def __init__(self) -> None:
-        self.post_processors = [replace_absolute_links]
+        self.post_processors = [
+            replace_absolute_links,
+            replace_pelican_links,
+        ]
 
     def process_page(self, page: Page) -> None:
         body = page.body or ''
